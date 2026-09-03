7e32046fad24ac25fec638443c5ea3b2a4b51f99
Bobby Lee <thayungbobby@gmail.com>
2026-09-03 08:45:13 -0400

    fixed missing import in file

--- frontend/src/pages/BookshelfDetailsPage/BookshelfDetailsPage.js
+++ frontend/src/pages/BookshelfDetailsPage/BookshelfDetailsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import Bookshelf from "../../components/Bookshelf/Bookshelf";
 
 const BookshelfDetailsPage = () => {
